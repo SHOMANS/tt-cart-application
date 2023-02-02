582bc1a4f23345de09f9d2c51034f92aeac6f33b
SHOMANS <mohammedshoman9@gmail.com>
2023-02-02 12:01:09 +0200

    create mock data

---
+++ src/mock/products.js
@@ -0,0 +1,7 @@
+import { generateId } from '../utils/generateId';
+
+export const products = [
+  { id: generateId(), name: 'product 1', price: 20, description: '' },
+  { id: generateId(), name: 'product 2', price: 30, description: '' },
+  { id: generateId(), name: 'product 3', price: 60, description: '' },
+];
